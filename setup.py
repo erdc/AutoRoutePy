cd291a89cdc4a9d8a5dbba15f37a792b2752562a
snowman2 <alan.d.snow@usace.army.mil>
2016-05-27 10:49:10 -0500

    added find_packages and vesion 2.1.0

--- setup.py
+++ setup.py
@@ -7,19 +7,26 @@
 ##  Copyright © 2015-2016 Alan D Snow. All rights reserved.
 ##  License: BSD-3 Clause
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='AutoRoutePy',
-    version='2.0.2',
+    version='2.1.0',
     description='Python scripting interface for the AutoRoute progam.'
                 ' Has ability to Prepare input from RAPID output (www.rapid-hub.org).',
     keywords='AutoRoute',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/AutoRoutePy',
-    download_url='https://github.com/erdc-cm/AutoRoutePy/tarballs/2.0.2',
+    download_url='https://github.com/erdc-cm/AutoRoutePy/archive/2.1.0.tar.gz',
     license='BSD 3-Clause',
-    packages=['AutoRoutePy'],
+    packages=find_packages(),
     install_requires=['condorpy', 'psutil', 'gdal', 'numpy', 'RAPIDpy'],
+    classifiers=[
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            ],
 )
