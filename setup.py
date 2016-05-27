7df44f7b58c3fcbacc1ff8d799c5b32a1af57682
Alan D. Snow <alan.d.snow@usace.army.mil>
2016-05-27 07:31:29 -0500

    Version to 2.0.2

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ from setuptools import setup
 
 setup(
     name='AutoRoutePy',
-    version='2.0.1',
+    version='2.0.2',
     description='Python scripting interface for the AutoRoute progam.'
                 ' Has ability to Prepare input from RAPID output (www.rapid-hub.org).',
     keywords='AutoRoute',
     author='Alan Dee Snow',
     author_email='alan.d.snow@usace.army.mil',
     url='https://github.com/erdc-cm/AutoRoutePy',
-    download_url='https://github.com/erdc-cm/AutoRoutePy/tarballs/2.0.1',
+    download_url='https://github.com/erdc-cm/AutoRoutePy/tarballs/2.0.2',
     license='BSD 3-Clause',
     packages=['AutoRoutePy'],
     install_requires=['condorpy', 'psutil', 'gdal', 'numpy', 'RAPIDpy'],
