4eb67da329439a439b4d03b334e99037a163a575
snowman2 <alan.d.snow@usace.army.mil>
2016-12-08 11:37:43 -0600

    added organize_dem script

--- AutoRoutePy/prepare/__init__.py
+++ AutoRoutePy/prepare/__init__.py
@@ -1,3 +1,4 @@
+from .organize_dem import organize_dem
 from .prepare import AutoRoutePrepare
 from .prepare_multiprocess import (prepare_autoroute_streamflow_single_folder,
                                    prepare_autoroute_single_folder,
